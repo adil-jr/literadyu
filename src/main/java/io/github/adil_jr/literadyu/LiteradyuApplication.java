28e39bb335b2c1c58be9cbc3a0878f9a04b6d9a6
adil-jr <adil.jr@outlook.com>
2025-06-10 01:49:48 -0300

    feat: initial project setup with dependencies

---
+++ src/main/java/io/github/adil_jr/literadyu/LiteradyuApplication.java
@@ -0,0 +1,13 @@
+package io.github.adil_jr.literadyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteradyuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteradyuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/adil_jr/literadyu/LiteradyuApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.adil_jr.literadyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteradyuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
